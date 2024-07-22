6a37a0cf5931fc3d2eba91007c593aab1bae90c0
jessica-pimentel <jppimentel96@hotmail.com>
2024-07-22 18:28:54 -0300

    Adicionando trabalho Aspectos Ageis de Programação

---
+++ AAP/BubbleSort.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+class BubbleSort {
+
+    static void bubbleSort(int[] array, int size) {
+        boolean swapped;
+        for (int i = 0; i < size - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < size - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (!swapped)
+                break;
+        }
+    }
+
+    static void printArray(int[] array, int size) {
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+        int size = array.length;
+        bubbleSort(array, size);
+        System.out.println("Array ordenado: ");
+        printArray(array, size);
+    }
+}
