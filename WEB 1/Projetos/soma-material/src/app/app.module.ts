ba55e7cc8e3d05d4cb3b76c34b80beff89e48160
jessica-pimentel <jppimentel96@hotmail.com>
2024-08-06 20:17:23 -0300

    criação de projetos com bootstrap e material design

---
+++ WEB 1/Projetos/soma-material/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { SomaModule } from './soma';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SomaModule
+  ],
+  providers: [
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
